64138f4a7815cd716a0acf7b690344a90b4c6ced
zhmi <zmmingtiandege1314@126.com>
2015-11-04 23:19:46 +0800

    Create two tables.One of the tables stores  the information of split words,and the other stores the information of the  raw data.Pass  parameter : database name and table name to the function.

--- sms_spam_classification/create_database_and_table.py
+++ sms_spam_classification/create_database_and_table.py
@@ -10,7 +10,7 @@
 # Author:          zhmi
 # E-mail:          zhmi120@sina.com
 # Create:          2015-11-3
-# Recent-changes:
+# Recent-changes:  2015-11-4
 
 ####################################### Part1 : Import  ################################################################
 
@@ -41,34 +41,96 @@ class createDatabaseTable(object):
             logging.info("start to connect the mysql")
             self.conn = MySQLdb.connect(host='localhost',user='root',passwd='95120',charset='utf8', port=3306)
             logging.info("connect successfully.")
-
         except Exception,ex:
             logging.info("fail to connect mysql")
         finally:
             pass
 
-    def createDatebase(self):
+    def createDatebase(self, database_name):
         try:
-            logging.info("create database sms_spam_classification_DB")
+            logging.info("start to create database : %s" %database_name)
             sqls=[" \
-                    CREATE DATABASE sms_spam_classification_DB \
-                    DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; \
-                 "]
+                    CREATE DATABASE IF NOT EXISTS %s \
+                    DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;\
+                  " %database_name]
             cursor = self.conn.cursor()
             cursor.execute(sqls[0])
-            logging.info("create database successfully.")
+            logging.info("create database %s successfully.",database_name)
         except Exception,ex:
             print Exception,"fail to create database",ex
         finally:
-            cursor.close()
-            self.conn.close()
-            logging.info("exit mysql successfully")
+            pass
+
+    def createTable(self, database_name, table_name_list):
+
+        cursor = self.conn.cursor()
+        sqls = ["use %s" %database_name,'SET NAMES UTF8']
+        sqls.append("""CREATE TABLE IF NOT EXISTS %s(
+                                        id INT(11) NOT NULL,
+                                        is_train INT(11),
+                                        true_label INT(11),
+                                        predicted_label INT(11),
+                                        is_spam_prob FLOAT,
+                                        word_num INT(11),
+                                        keyword1 TEXT,
+                                        keyword2 TEXT,
+                                        keyword3 TEXT,
+                                        content TEXT NOT NULL,
+                                        split_result_string TEXT,
+                                        split_result_num INT(11),
+                                        split_result_clean_string TEXT,
+                                        split_result_clean_num INT(11),
+                                        stopword_num INT(11),
+                                        word_index_string TEXT,
+                                        word_vector_string TEXT,
+                                        UNIQUE (id))""" % table_name_list[0])
+
+        sqls.append("CREATE INDEX id_idx ON %s(id)" % table_name_list[0])
+
+        # table_name_list[1]: word_table
+        sqls.append("""CREATE TABLE IF NOT EXISTS %s(
+                                id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                                word VARCHAR(100),
+                                is_stopword INT(11),
+                                word_length INT(11),
+                                topic TEXT,
+                                true_pos_num INT(11),
+                                true_neg_num INT(11),
+                                all_num INT(11),
+                                true_neg_pro FLOAT,
+                                predicted_pos_num INT(11),
+                                predicted_neg_num INT(11),
+                                UNIQUE (word))""" % table_name_list[1])
+
+        try:
+            for i in xrange(len(sqls)):
+                cursor.execute(sqls[i])
+            logging.info("create table successfully")
+        except Exception, ex:
+            print Exception, "fail to create table", ex
+        finally:
+            pass
+
+
+
+    def close_database(self):
+        cursor = self.conn.cursor()
+        cursor.close()
+        self.conn.close()
+        logging.info("exit mysql successfully")
+
 
 ####################################### Part4 : Test  ##################################################################
 
+name = "spm_message_information_DB"
+table_list = ['message_information','word_infomation']
 Creater = createDatabaseTable()
 Creater.connectMysql()
-Creater.createDatebase()
+Creater.createDatebase(database_name = name)
+Creater.createTable(database_name = name, table_name_list = table_list)
+
+
+
 
 
 
