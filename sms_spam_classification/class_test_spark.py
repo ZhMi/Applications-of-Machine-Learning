1b36851d4cc55a87918461bf3f2e09ad2c5fae48
zhmi <zmmingtiandege1314@126.com>
2015-11-10 22:07:51 +0800

    Because the computer can not eazily process the calculation whit large data.So I  set up and config spark.And then  begin to use spark rewrite my program. This file is written to test whether the pycharm could use spark correctly

---
+++ sms_spam_classification/class_test_spark.py
@@ -0,0 +1,34 @@
+#-*- coding:utf8 -*-
+#!usr/bin/python
+
+####################################### PART0 Description ##############################################################
+
+# Filename:        test_spark.py
+# Description:     when the data is vary large,I can not run my program.And the computer is becoming slowly,it even
+#                  stops running.So I start to use spark during the codes.I have config spark in my computer.
+#                  This file is in order to test whether pycharm could use spark.
+#                  could use spark.
+#
+#
+# Author:          zhmi
+# E-mail:          zhmi120@sina.com
+# Create:          2015-11-10
+# Recent-changes:
+
+####################################### PART1 Class ####################################################################
+
+from pyspark import SparkContext
+
+class testSpark:
+    def __init__(self):
+        pass
+    def test_spark_could_run(self):
+        sc = SparkContext("local", "test")
+        rdd = sc.parallelize([1, 2, 3, 4])
+        print "rdd.collect():%s", str(rdd.collect())
+        sc.stop()
+
+####################################### PART2 Test   ###################################################################
+
+testObject = testSpark()
+testObject.test_spark_could_run()
