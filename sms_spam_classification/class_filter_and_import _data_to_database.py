90132546f6ef1fba393850d994a4ec9ebf7ee442
zhmi <zmmingtiandege1314@126.com>
2015-11-15 22:09:03 +0800

    1.use jieba cut word successfully ,but only saves 100000 pieces of  records, not 800000 pieces of records 2.get length of the list has some problems 3.use union get all of the elems of rdd1 and rdd2

--- sms_spam_classification/class_filter_and_import _data_to_database.py
+++ sms_spam_classification/class_filter_and_import _data_to_database.py
@@ -35,6 +35,7 @@ class filterDataFun(object):
         self.rdd_raw_File = self.sc\
                             .textFile(self.data_file_dir) \
                             .map(lambda x: x.strip())
+        self.raw_File_list = self.rdd_raw_File.collect()
         return self.rdd_raw_File
 
     def seprateLine(self):
@@ -46,19 +47,38 @@ class filterDataFun(object):
                                      'rdd_raw_list_six',
                                      'rdd_raw_list_seven',
                                      'rdd_raw_list_eight']
+
         self.rdd_raw_message_dict = {}
         for i in xrange(len(self.rdd_raw_message_name_list)):
+            sequence = self.raw_File_list[100000*i:100000*(i+1)]
             self.rdd_raw_message_dict[self.rdd_raw_message_name_list[i]] \
                 = self.sc \
-                      .parallelize(self.rdd_raw_File.collect()[100000*i:100000*(i+1)]) \
-                      .map(lambda x: (x.split("\t"), list(jieba.cut(x[4:]))))
+                      .parallelize(sequence) \
+                      .map(lambda x: [x.split("\t")])
 
+            # self.rdd_message_content = self.rdd_raw_File.map(lambda x: x[4:])
+            # self.rdd_cut_word_list = self.rdd_message_content.map(lambda x: list(jieba(x)))
+            # test version
+            self.rdd_message_content = self.rdd_raw_message_dict[self.rdd_raw_message_name_list[i]] \
+                                           .map(lambda x: x[0][-1:-2:-1])
+
+            # if I write last  sentence as map(lambda x: x[2]) ,there will be an error
+
+            self.rdd_cut_word_list = self.rdd_message_content.map(lambda x: [ i for i in jieba.cut(x[0])])
+            # list(jieba.cut(message)
+            self.rdd_cut_word_count_list = self.rdd_cut_word_list.map(lambda x: len(x))
             # create eight rdds of rdd_raw_list , every rdd contains 100,000 pieces of massage
         logging.info("create dict rdd_raw_message_dict, key is name = rdd ,value is rdd content")
         return self.rdd_raw_message_dict
 
     def getDictKey(self):
         return self.rdd_raw_message_name_list
+
+    def getCutWord(self):
+        return self.rdd_cut_word_list
+
+    def getCutWordCount(self):
+        return self.rdd_cut_word_list
 
     def stop(self):
         self.sc.stop()
@@ -75,7 +95,7 @@ raw_data_list = testObject.readFile()
 
 rdd_raw_message_dict = testObject.seprateLine()
 
-#[text : create eight rdd of raw data]
+# [text : create eight rdd of raw data]
 
 key_list = testObject.getDictKey()
 
@@ -84,9 +104,38 @@ for i in xrange(len(key_list)):
     print "id", i, "***", "length :", len(temp_list)
     print "elem of head list:"
     for k in temp_list[0:5]:
-        print k
+        print k[0][0], "**", k[0][1], "***", k[0][2], "****"
     print "elem of tail list:"
     for k in temp_list[-1:-6:-1]:
-        print k
+        print k[0][0], "**", k[0][1], "***", k[0][2], "****"
+
+# [test word cut function]
+
+word_cut_list = testObject.getCutWord().collect()
+print "length of word_cut_count_list: ", len(word_cut_list)
+
+print "elem of head list:"
+
+
+for k in word_cut_list[0:5]:
+    for q in k:
+        print q
+    print("********************")
+print "elem of tail list:"
+for k in word_cut_list[-1:-6:-1]:
+    for q in k:
+        print q
+    print("********************")
+# [test word cut numbers count part ]
+
+word_cut_count_list = testObject.getCutWordCount().collect()
+print "length of word_cut_count_list: ", len(word_cut_count_list)
+
+print "elem of head list:"
+for k in word_cut_count_list[0:5]:
+    print k
+print "elem of tail list:"
+for k in word_cut_count_list[-1:-6:-1]:
+    print k
 
 testObject.stop()
