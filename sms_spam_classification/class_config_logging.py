5876037cde9f905dd02e062378c19092fc831d66
zhmi <zmmingtiandege1314@126.com>
2015-11-24 21:15:09 +0800

    write variables into a file

--- sms_spam_classification/class_config_logging.py
+++ sms_spam_classification/class_config_logging.py
@@ -20,20 +20,18 @@ import logging
 ####################################### Part1 : Inti  ################################################################
 
 class packageLogging(object):
-
     def __init__(self):
-        print "start to initiallize the packageLogging class"
-
+        pass
     def configLoggingFun(self):
         print "start to config logging"
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                             datefmt='%a, %d %b %Y %H:%M:%S',
                             filename='./main.log',
-                            filemode='a+')
+                            filemode='w')
         console = logging.StreamHandler()
         console.setLevel(logging.INFO)  # 设置日志打印格式
-        formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+        # formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+        formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
         console.setFormatter(formatter)  # 将定义好的console日志handler添加到root logger
         logging.getLogger('').addHandler(console)
-        print "finish configging logging"
